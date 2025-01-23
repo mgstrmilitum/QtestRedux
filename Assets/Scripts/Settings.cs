d13df6bbeada4f8c2238262749800ade43328a00
Luke Monesmith <LDMonesmith@student.fullsail.edu>
2025-01-23 14:53:13 -0500

    Main Menu and in game options sync

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -45,10 +45,12 @@ public class Settings : MonoBehaviour
     public void LoadSettings()
     {
         mouseSens = PlayerPrefs.GetFloat("mouseSens", 2);
+        sensSlider.value = mouseSens;
+
         invertAxis = (PlayerPrefs.GetInt("invertAxis") != 0);
-        
         if (invertAxis == true) { invertButton.gameObject.SetActive(true); }
-       else if (invertAxis == false) { invertButton.gameObject.SetActive(false); }
+        else if (invertAxis == false) { invertButton.gameObject.SetActive(false); }
+
 
 
     }
