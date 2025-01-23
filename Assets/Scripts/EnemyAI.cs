46199e6b198a28fb0a147ae5b08822f160871136
Dylan-Ll-T <Dyllanostorres@student.fullsail.edu>
2025-01-23 18:09:22 -0500

    Bunch of Code Cleanup

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -5,31 +5,35 @@ using UnityEngine;
 using UnityEngine.AI;
 using static Photon.Pun.UtilityScripts.PunTeams;
 
+/*GENERAL COMMENTS*/
+//-------------------
+// -Hellhounds Should Probably be another script.
+//-------------------
+
+
+
 public class EnemyAI : MonoBehaviour, IDamage
 {
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Renderer model;
     [SerializeField] int health;
+
+
     [SerializeField] int faceTargetSpeed;
     [SerializeField] Transform shotPosition;
     [SerializeField] GameObject bullet;
     [SerializeField] float rateOfFire;
-    [SerializeField] int team;
 
     Color origColor;
     bool isShooting;
     bool playerInRange;
-    bool enemyInRange;
+    
 
     Vector3 playerDirection;
-    Vector3 enemyDirection;
+    
 
     // Roaming
     public Vector3 walkPoint;
-    bool walkPointSet;
-    public float rangeToWalkPoint;
-    LayerMask ground;
-
 
     // Start is called before the first frame update
     void Start()
@@ -44,57 +48,18 @@ public class EnemyAI : MonoBehaviour, IDamage
     {
         if (playerInRange)
         {
-            enemyDirection = GameManager.Instance.player.transform.position;
-
-            //if (agent.remainingDistance <= agent.stoppingDistance)
-            //{
-            //    FaceTarget();
-            //}
-
-            //if (!isShooting)
-            //{
-            //    StartCoroutine(Shoot());
-            //}
-
-            TargetPlayerOrEnemy();
+            playerDirection = GameManager.Instance.player.transform.position;
+            TargetPlayer();
         }
-        else if (enemyInRange)
-        {
-            enemyDirection = GameManager.Instance.player.transform.position;
-
-            //Probs gotta tune gameManager to be able to recognize an enemy.
-            //playerDirection = GameManager.Instance.player.transform.position - transform.position;
-
-            //agent.SetDestination(GameManager.Instance.player.transform.position);
-
-            //if (agent.remainingDistance <= agent.stoppingDistance)
-            //{
-            //    FaceTarget();
-            //}
-
-            //if (!isShooting)
-            //{
-            //    StartCoroutine(Shoot());
-            //}
-
-            TargetPlayerOrEnemy();
-        }
-        //else
-        //{
-        //    Roam();
-        //}
+        
     }
-
+    
+    // Detects Player when he enters the
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             playerInRange = true;
-        }
-        else if (other.CompareTag("Enemy"))
-        {
-            EnemyAI otherEnemy = other.GetComponent<EnemyAI>();
-            if (otherEnemy != null && otherEnemy.team != team) { enemyInRange = true; }
         }
     }
 
@@ -104,16 +69,26 @@ public class EnemyAI : MonoBehaviour, IDamage
         {
             playerInRange = false;
         }
-        else if (other.CompareTag("Enemy"))
+    }
+
+    private void TargetPlayer()
+    {
+        agent.SetDestination(playerDirection - transform.position);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
         {
-            EnemyAI otherEnemy = other.GetComponent<EnemyAI>();
-            if (otherEnemy != null && otherEnemy.team != team) { enemyInRange = false; }
+            FaceTarget();
+        }
+
+        if (!isShooting)
+        {
+            StartCoroutine(Shoot());
         }
     }
 
     void FaceTarget()
     {
-        Quaternion rot = Quaternion.LookRotation(enemyDirection);
+        Quaternion rot = Quaternion.LookRotation(playerDirection);
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
     }
 
@@ -132,9 +107,7 @@ public class EnemyAI : MonoBehaviour, IDamage
     IEnumerator FlashRed()
     {
         model.material.color = Color.red;
-
         yield return new WaitForSeconds(0.15f);
-
         model.material.color = origColor;
     }
 
@@ -144,45 +117,5 @@ public class EnemyAI : MonoBehaviour, IDamage
         Instantiate(bullet, shotPosition.position, transform.rotation);
         yield return new WaitForSeconds(rateOfFire);
         isShooting = false;
-    }
-
-    public void Roam()
-    {
-        if (!walkPointSet) SearchForWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        if (distanceToWalkPoint.magnitude <= 1f)
-            walkPointSet = false;
-
-    }
-
-    private void SearchForWalkPoint()
-    {
-        float randX = UnityEngine.Random.Range(-rangeToWalkPoint, rangeToWalkPoint);
-        float randZ = UnityEngine.Random.Range(-rangeToWalkPoint, rangeToWalkPoint);
-
-        walkPoint = new Vector3(transform.position.x + randX, transform.position.y, transform.position.z + randZ);
-
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, ground))
-            walkPointSet = true;
-    }
-
-    private void TargetPlayerOrEnemy()
-    {
-        agent.SetDestination(enemyDirection - transform.position);
-
-        if (agent.remainingDistance <= agent.stoppingDistance)
-        {
-            FaceTarget();
-        }
-
-        if (!isShooting)
-        {
-            StartCoroutine(Shoot());
-        }
     }
 }
